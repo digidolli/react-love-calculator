1d03bd433d8bcc2892fadda7119caeff172a8d79
Digidolli <iamsadekaye@gmail.com>
2022-02-27 15:43:08 +0000

    Added forms, styled with css.
    - Started storing entry from input.
    - Added conditional to change display and display results.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+import React, { useState } from 'react';
+
+
 import './App.css';
 
 function App() {
+
+  let [yourName, setYourName] = useState (null)
+  let [theirName, setTheirName] = useState (null)
+  let [calculated, setCalculated] = useState ({ready: false})
+  
+  function handleSumbit(event){
+    event.preventDefault();
+    setCalculated ({
+      ready: true,
+      percentage: Math.floor(Math.random()),
+    })
+  }
+
+  function handleYourNameChange (event){
+    setYourName(event.target.value)
+    console.log(yourName)
+  }
+
+  function handleTheirNameChange (event){
+    setTheirName(event.target.value)
+    console.log(theirName)
+  }
+  
+  if (calculated.ready){
+    return (
+      <div className='App calculated'>
+        <h1>{yourName} + {theirName}</h1>  
+      </div>
+    )
+  } else {
   return (
     <div className="App">
-     
-    </div>
-  );
+      <h1>Love calculator</h1>     
+        <form className="yourName" onSubmit={handleSumbit}>
+            <input type="search" placeholder="enter your name..." onChange={handleYourNameChange} /> 
+            <input type="search" placeholder='enter their name...' onChange={handleTheirNameChange}/> 
+            <input type="submit" value="Calculate"/>
+        </form>
+       </div>
+  )}
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <footer>
+        This project was coded by <a href="https://www.instagram.com/digidolli/" target="_blank" rel="noreferrer">digidolli</a> and is <a href="https://github.com/digidolli/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github </a> 
+      </footer>
   </React.StrictMode>,
   document.getElementById('root')
 );
