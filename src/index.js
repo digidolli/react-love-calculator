149b0c50b0fef54e07da013d064ec2504c180d68
Digidolli <iamsadekaye@gmail.com>
2022-02-27 22:55:03 +0000

    Changes Git link and padding

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
     <footer>
-        This project was coded by <a href="https://www.instagram.com/digidolli/" target="_blank" rel="noreferrer">digidolli</a> and is <a href="https://github.com/digidolli/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github </a> 
+        This project was coded by <a href="https://www.instagram.com/digidolli/" target="_blank" rel="noreferrer">digidolli</a> and is <a href="https://github.com/digidolli/react-love-calculator" target="_blank" rel="noreferrer"> open-sourced on Github </a> 
       </footer>
   </React.StrictMode>,
   document.getElementById('root')
